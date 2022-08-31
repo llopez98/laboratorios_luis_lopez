c7793fab7d53bade27436504a8f4b162b808d2be
Luis Antonio Lopez <lueunbii98@gmail.com>
2022-08-30 19:58:55 -0500

    Laboratorio 1 30-08-2022

---
+++ lab1/lab11.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>laboratorio 1.1</title>
+    </head>
+    <body>
+        <?php 
+            $ini = "Hola";
+            $fin = " a todos";
+            $todo = $ini.$fin;
+            echo $todo;
+        ?>
+    </body>
+</html>
